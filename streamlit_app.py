fba29b23a90ed6c5ed3d113c732d62148a8117ef
Breno Akihiro Morimoto <77297174+brenoakihiromorimoto@users.noreply.github.com>
2024-08-11 21:45:59 -0300

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,27 +7,28 @@ import json
 import time
 import re
 
-#
+#Fill null value to avoid error on parameter 'sciences' of function 'generate_matrix_baseline'
 if "options_sciences" not in st.session_state:
     st.session_state.options_sciences = "None"
-
+#Fill null value to avoid error on parameter 'industry' of function 'generate_matrix_baseline'
 if "options_industries" not in st.session_state:
     st.session_state.options_industries = "None"
-
+#Fill null value to avoid error on solution's text
 if "solutions" not in st.session_state:
     st.session_state.solutions = "None"
 
+#Clean Gemini API Text to create a better UX Writing.
 def clean_text(text):
     # Remove special caracters to User could read more easly.
     text = re.sub(r'[^a-zA-Z0-9\s://.;-]', '', text)
     text = text.strip()
     return text
 
-#Generate a strutured solution.
+#Generate a table to serve like a 'reasoning' to Gemini. Suggestions of solutions would be based on that.
 def generate_matrix_baseline(
         num_col=3, 
         num_rows=3, 
-        product='Solar Panel', 
+        problem='None', 
         sciences="None", 
         industry="None"
         ):
@@ -40,7 +41,7 @@ def generate_matrix_baseline(
     )
 
     prompt = f"""
-    Generate {num_col} Critical Functions and {num_rows} Features for each Critical Function of {product} on {industry}.
+    Generate {num_col} Critical Functions and {num_rows} Features for each Critical Function of {problem} on {industry}.
     Adapt features of {sciences}. Maximum 3 words.
     Using this Python Dictionary schema:
       {{
@@ -57,7 +58,8 @@ def generate_matrix_baseline(
     response_prep_02 = response_prep_01.text.strip().replace("\n", "").replace("python", "").replace("```", "")
     st.session_state['table'] = response_prep_02
 
-def generate_solutions_baseline():
+#Use 'st.session_state['table']' to generate suggestion of solution.
+def generate_solutions_baseline(problem="None"):
     genai.configure(api_key='AIzaSyDiQBWIfHTto65mCYu0EUxPhlBlfVxBp-I')
 
     # Using `response_mime_type` with `response_schema` requires a Gemini 1.5 Pro model
@@ -66,16 +68,16 @@ def generate_solutions_baseline():
     )
 
     prompt = f"""
-    Generate 1 suggestion of solutions with data bellow.
+    Generate 1 suggestion of solution with data bellow.
     {st.session_state['table']}
 
     Important: you need to apply morphological analysis. Follow template bellow with maximum 50 words.
 
     Template of output:
-    Problem: [Describe problem of somebody]
+    Problem: {problem}
     Solution: [Title]
-    How it works: [explain your morphological analysis. Maximum 150 words]
-    References: [Get 3 references from Web to explain how it works. Maximum 10 words]
+    How it works: [explain your morphological analysis. Minimum 300 words to Maximum 700 words]
+    References: [Get 3 references from Web to explain how it works. Maximum 10 words. Remove any link]
     - Reference 01:
     - Reference 02:
     - Reference 03:
@@ -87,6 +89,9 @@ def generate_solutions_baseline():
     st.session_state['solutions'] = df_response
 
 with st.sidebar:    
+    #Branding Awarenness
+    st.header("Franklin.AI")
+    #More technical areas to create robust solutions.
     st.session_state.options_science = st.multiselect(
     "Give me your research areas to adapt your product",
     ["Agricultural Technology (AgTech)", "Blockchain Technology", "Biotechnology", 
@@ -112,20 +117,27 @@ with st.sidebar:
         generate_matrix_baseline(
             num_col=5, 
             num_rows=5, 
-            product=st.session_state.input,
+            problem=st.session_state.input,
             sciences=st.session_state.options_science, 
             industry=st.session_state.options_industries
             )
         #Step 02: generate a text with more meaningful solutions
-        generate_solutions_baseline()
+        generate_solutions_baseline(problem=st.session_state.input)
 
-    input = st.text_input('Input any idea or theme, It will be adapted anyway:', 
-                          key='input', placeholder='eg. Solar Energy')
+    input = st.text_input('What is the main problem to solve?', 
+                          key='input', placeholder='eg. Segmentation of Leads to receive Credit')
     
     button_generate = st.button("Generate new solution", type="primary", on_click=first_matrix)
 
 with st.container(border=True):
     solutions = st.session_state.solutions
+    if "None" in st.session_state.solutions:
+        st.header("Empowering innovation teams with rapid idea generation for breakthrough solutions.", divider=True)
+        st.subheader("Instructions:")
+        st.text("you need to fill:")
+        st.text("1: research areas related to the product.")
+        st.text("2: industry is related to your product.")
+        st.text("3: context (theme/problem/constraints) to generate great suggestions.")
 
     if "None" != st.session_state.solutions:
 
@@ -136,10 +148,10 @@ with st.container(border=True):
         st.header("Problem: " + header, divider=True)
         st.subheader("Solution: " + subheader, divider=True)
 
-        text = clean_text(solutions.split('Problem:')[1].split("How it works:")[1].split("- Reference 01:")[0])
+        text = clean_text(solutions.split('Problem:')[1].split("How it works:")[1].split("- Reference 01:")[0].replace("References:", ""))
         st.write(text)
 
-        st.text("References:")
+        st.text("Search References:")
 
         references = [i.strip() for i in clean_text(solutions.split('- Reference 01:')[1]).split('- ') if i!=""]
 
